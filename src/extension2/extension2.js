fbf60f7df295e6e7b862ba4dcb5bfe5c34af3102
sandyatech <sandhya.mskgmail.com>
2023-02-10 12:03:35 +0000

    added extension2:arrow functions definitions

---
+++ src/extension2/extension2.js
@@ -0,0 +1,22 @@
+// Instructions can be found in functions.md
+// Function Declarations
+// logPerson() is a "Function Declaration"
+// Note how it uses the "function" keyword at the beginning of it's declaration
+// This is the original way of writing functions in JavaScript
+var people = ["Harry", "Hermione", "Ron"];
+function logPerson(person) {
+    console.log("The person is " + person);
+}
+people.forEach(logPerson);
+// Function in a Variable
+// Store the logPerson() function in a variable called performLogPerson
+// Your code here
+var performLogPerson = function logPerson(person) { console.log("The person is " + person); };
+// Arrow Functions in a Variable
+// Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
+// Your code here
+var arrowVersionOfLogPerson = function (person) { console.log("The person is " + person); };
+// Anonymous Arrow Functions
+// Replace logPerson below with an Anonymous Arrow Function that does the same thing!
+people.forEach(function (person) { console.log("The person is " + person); });
+// In JavaScript there are lots of ways to write functions. There is no right or wrong way, but arrow functions are the most modern. 🙂
---
+++ src/extension2/extension2.ts
@@ -0,0 +1,17 @@
+﻿const people = ["Harry", "Hermione", "Ron"];
+
+function logPerson(person:string) {
+    console.log("The person is " + person);
+}
+
+people.forEach(logPerson);
+
+
+const performLogPerson = function logPerson(person:string) { console.log("The person is " + person); };
+
+
+const arrowVersionOfLogPerson = (person:string):void => { console.log("The person is " + person); };
+
+
+people.forEach(person => { console.log("The person is " + person); });
+
